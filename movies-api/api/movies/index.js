40c630465fe6a8d1de24bda38a1eb5f00bfe5b18
Kia,Conaty Carley 20088426 <20088426@wit.ie>
2022-05-02 21:32:12 +0100

    Topic 11 - Finished!

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -3,6 +3,7 @@ import { movies, movieReviews, movieDetails } from './moviesData';
 import uniqid from 'uniqid';
 import movieModel from './movieModel';
 import asyncHandler from 'express-async-handler';
+import { getUpcomingMovies } from '../tmdb-api';
 
 
 app.use(express.json());
@@ -69,6 +70,11 @@ router.get('/:id/reviews', (req, res) => {
     }
 });
 
+
+router.get('/tmdb/upcoming', asyncHandler( async(req, res) => {
+    const upcomingMovies = await getUpcomingMovies();
+    res.status(200).json(upcomingMovies);
+  }));
 
 
 export default router;
