0e88d71fde775902477c205dafa736e2cc472102
Kia,Conaty Carley 20088426 <20088426@wit.ie>
2022-05-02 20:53:35 +0100

    Topic 10 - Object Referencing

--- movies-api/api/users/index.js
+++ movies-api/api/users/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import User from './userModel';
 import asyncHandler from 'express-async-handler';
 import jwt from 'jsonwebtoken';
+import movieModel from '../movies/movieModel';
 
 const router = express.Router(); // eslint-disable-line
 
@@ -30,38 +31,21 @@ router.post('/', async (req, res) => {
     }
 });
 
-router.post('/',asyncHandler( async (req, res, next) => {
-    if (!req.body.username || !req.body.password) {
-      res.status(401).json({success: false, msg: 'Please pass username and password.'});
-      return next();
-    }
-    if (req.query.action === 'register') {
-      await User.create(req.body);
-      res.status(201).json({code: 201, msg: 'Successful created new user.'});
-    } else {
-      const user = await User.findByUserName(req.body.username);
-        if (!user) return res.status(401).json({ code: 401, msg: 'Authentication failed. User not found.' });
-        user.comparePassword(req.body.password, (err, isMatch) => {
-          if (isMatch && !err) {
-            // if user is found and password matches, create a token
-            const token = jwt.sign(user.username, process.env.SECRET);
-            // return the information including token as JSON
-            res.status(200).json({success: true, token: 'BEARER ' + token});
-          } else {
-            res.status(401).json({code: 401,msg: 'Authentication failed. Wrong password.'});
-          }
-        });
-      }
+router.post('/:userName/favourites', asyncHandler(async (req, res) => {
+    const newFavourite = req.body.id;
+    const userName = req.params.userName;
+    const movie = await movieModel.findByMovieDBId(newFavourite);
+    const user = await User.findByUserName(userName);
+    await user.favourites.push(movie._id);
+    await user.save(); 
+    res.status(201).json(user); 
   }));
 
-router.get('/:id/favourites', async (req, res) => {
-    const user = await User.findById(req.params.id);
-    if (user) {
-        res.status(200).json(user.favourites);
-    } else {
-        res.status(404).json({ code: 404, msg: 'Unable to find favourites' });
-    }
-});
+  router.get('/:userName/favourites', asyncHandler( async (req, res) => {
+    const userName = req.params.userName;
+    const user = await User.findByUserName(userName).populate('favourites');
+    res.status(200).json(user.favourites);
+  }));
 
 router.put('/:id', async (req, res) => {
     if (req.body._id) delete req.body._id;
